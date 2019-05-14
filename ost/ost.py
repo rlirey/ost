86c455378dc015ffc75b50024b2d73f58c7f08b2
Ryan Irey <rlirey@gmail.com>
2019-05-14 10:09:54 -0500

    update ost.py and utilities.py

--- ost/ost.py
+++ ost/ost.py
@@ -11,15 +11,18 @@ class OST(object):
     global session
     global token
 
-    def __init__(self):
-        pass
 
-    def download(self, save_path, **kwargs):
+    def __init__(self, read_path, save_path):
+        self.read_path = read_path
+        self.save_path = save_path
+
+
+    def download(self, **kwargs):
         response = requests.get(self.url)
         compressed_file = BytesIO(response.content)
         decompressed_file = GzipFile(fileobj=compressed_file)
         srt2 = rename_srt(self.file)
-        with open(save_path + srt2, 'wb') as output:
+        with open(self.save_path + srt2, 'wb') as output:
             output.write(decompressed_file.read())
         return "Subtitles downloaded!"
 
@@ -31,17 +34,20 @@ class TV(OST):
         self.file = file
         se_info = get_se_info(self.file)
         f = File(self.file)
-        hash_file = f.get_hash()
-        data = get_sub_data(hash_file)
+        self.hash_file = f.get_hash()
+        data = get_sub_data(self.hash_file)
         if len(data) > 0:
             accurate = []
+            p = re.compile('\w\d\d\w\d\d', re.IGNORECASE)
             for i in range(len(data)):
-                if se_info in data[i]['SubFileName'].split("."):
+                if se_info[0] in p.findall(data[i]['SubFileName']) or se_info[1] in p.findall(data[i]['SubFileName']):
                     accurate.append(i)
-            self.url = data[accurate[0]].get("SubDownloadLink")
-            self.srt = data[accurate[0]].get("SubFileName")
-            return "Subtitles found!"
-        return file.split("/")[-1].split(".")[0] + ": subtitles not found!"
+            if len(accurate):
+                self.url = data[accurate[0]].get("SubDownloadLink")
+                self.srt = data[accurate[0]].get("SubFileName")
+                print("Subtitles found!")
+        else:
+            print(file + ": subtitles not found!")
 
 
 class MOVIE(OST):
@@ -50,21 +56,10 @@ class MOVIE(OST):
     def movie_search(self, file, **kwargs):
         self.file = file
         f = File(file)
-        hash_file = f.get_hash()
-        data = get_sub_data(hash_file)
+        self.hash_file = f.get_hash()
+        data = get_sub_data(self.hash_file)
         if len(data) > 0:
             self.url = data[0].get("SubDownloadLink")
             self.srt = data[0].get("SubFileName")
             return "Subtitles found!"
         return "Subtitles not found!"
-
-
-
-
-
-
-
-
-
-
-
--- ost/utilities.py
+++ ost/utilities.py
@@ -4,11 +4,20 @@ import requests
 from io import BytesIO
 from glob import glob
 from gzip import GzipFile
+# from xmlrpc.client import ServerProxy, Transport
 
 
 ##################################################################
 ###################  Utility/Helper Functions  ###################
 ##################################################################
+
+# url = 'http://api.opensubtitles.org/xml-rpc'
+# server = ServerProxy(url)
+# token = server.LogIn('rlirey@gmail.com', '443182lotr', 'eng', 'Butter v1')['token']
+# sub_id = 1952848198
+# resp = server.DownloadSubtitles(token, [1952848198])
+
+# server.CheckMovieHash(token, ['fad1a77982bab11e'])
 
 
 session = OpenSubtitles()
@@ -20,11 +29,19 @@ def rename_srt(file):
 
 
 def get_se_info(file):
-    se_info = file.split("/")[-1].split(".")[0].split(" ")[-1]
-    return se_info
+    import re
+    p = re.compile('\w\d\d\w\d\d', re.IGNORECASE)
+    se_info = p.findall(file)[0]
+    return (se_info.upper(), se_info.lower())
 
 
 def get_sub_data(hash_file):
     global session
     data = session.search_subtitles([{'sublanguageid':'eng', 'moviehash':hash_file}])
+    return data
+
+
+def get_sub_id(hash_file):
+    global session
+    data = session.search_subtitles([{'moviehash':hash_file}])
     return data
